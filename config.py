854fe3041282fdeaccc4914abbde682c386dc42a
Pittow <rdem4209@gmail.com>
2024-09-13 12:02:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-TELEGRAM_BOT_TOKEN = ""
-RPC_URL = ""
+TELEGRAM_BOT_TOKEN = "6866457962:AAFRRkJ88NIC883dqvAI-Og9fWSVUqdqcX8"
+RPC_URL = "https://green-responsive-bird.solana-mainnet.quiknode.pro/8588c344bbbbc1f0495f5d98dd3b1349ef79a9a9"
